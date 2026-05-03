e1b87c5986041e8902957af9bc6a6f63aaa98ad6
Omi <seyam.islam020@gmail.com>
2026-05-03 16:30:44 +0600

    added not found page and basic package installed

---
+++ src/app/not-found.jsx
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+import React from 'react'
+
+const NotFoundPage = () => {
+  return (
+    <div>
+        
+        <h1>Not Found</h1>
+
+        <Link href={'/'}>Back to Home</Link>
+      
+    </div>
+  )
+}
+
+export default NotFoundPage
